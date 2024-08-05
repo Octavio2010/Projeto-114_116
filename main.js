d220f3977f1ea6827d1f8f6c47eb896f08493824
Octavio2010 <131196314+Octavio2010@users.noreply.github.com>
2024-08-05 18:29:45 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+noseX = ""
+noseY = ""
+
 function preload(){
     
 }
@@ -6,7 +9,25 @@ function setup(){
     canva.center()
     cam = createCapture(VIDEO);
     cam.hide()
+    poseNet = ml5.poseNet(cam, modelLoaded)
+    poseNet.on("pose", gotPoses)
 }
+
+function modelLoaded(){
+    console.log("modelo carregado")
+}
+
+
 function draw(){
     image(cam, 0, 0, 650, 490)
+    fill("blue") 
+    circle(noseX, noseY, 6)
+}
+
+function gotPoses(results){
+    if(results.length>0){
+    console.log (results)
+    noseX = results[0].pose.nose.x
+    noseY = results[0].pose.nose.y
+    }
 }
